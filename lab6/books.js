9ffb99ce71e004968f3504260654e3406d6ebb81
Дмитрий Игуменшев <ddmauushy@gmail.com>
2025-05-19 21:42:02 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 500,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 800,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 1200,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Дейл Карнеги',
+        name: 'Как завоевывать друзей и оказывать влияние на людей',
+        publicationYear: 1936,
+        price: 450,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Карл Маркс',
+        name: 'Капитал',
+        publicationYear: 1867,
+        price: 790,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Роберт Чалдини',
+        name: 'Психология влияния',
+        publicationYear: 1984,
+        price: 1000,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Эрик Берн',
+        name: 'Игры, в которые играют люди',
+        publicationYear: 1964,
+        price: 650,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Харвилл Хендрикс',
+        name: 'Любовь на всю жизнь: Руководство для пар',
+        publicationYear: 1988,
+        price: 700,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Эрих Фромм',
+        name: 'Искусство любить',
+        publicationYear: 1956,
+        price: 420,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Джейн Остен',
+        name: 'Гордость и предубеждение',
+        publicationYear: 1813,
+        price: 280,
+        bestseller: true,
+        available: true
+    }
+];
